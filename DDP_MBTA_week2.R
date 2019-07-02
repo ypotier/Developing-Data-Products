59edd926554a4b7cbd7862b83dca89525593ab58
Yohann Potier <yohann.potier@novartis.com>
2019-07-02 11:58:45 -0400

    Use relative path with here package instead of full path for MBTA file

--- DDP_MBTA_week2.R
+++ DDP_MBTA_week2.R
@@ -7,17 +7,20 @@
 # MBTA developer tools: https://www.mbta.com/developers 
   
 library(leaflet)
+library(here)
 
 # Map the Boston Area
 my_map <- leaflet() %>%
   addTiles() %>%
-  setView(lng=-71.0589, lat=42.3601 , zoom=12) #  Boston coordinates
+  setView(lng=-71.0589, lat=42.3601 , zoom=12) #  Boston coordinates and zoom around Boston metropole only
 my_map
 
 # Load MBTA stops data
-stops = read.csv("/Users/potieyo1/Documents/Development/Classes/MBTA_GTFS/stops.txt")
-# since the total size is more than 9000 points and it takes time to render, I create a smaller dataset of 1000 rows
-sample_data <- stops[c(1:1000),]
+# Using here package for relative path instead of hardcoding mine
+mbta_stops=here::here("MBTA_GTFS", "stops.txt")
+stops = read.csv(mbta_stops)
+# since the total size is more than 9000 points and it takes time to render, I create a smaller dataset of 500 rows
+sample_data <- stops[c(1:500),]
 
 
 my_map <- leaflet(data = sample_data) %>%
